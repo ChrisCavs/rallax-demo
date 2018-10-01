3355798b73ae225a60d1afc082173d4ee3c35c74
Chris <chris211321@gmail.com>
2018-10-01 18:14:32 -0400

    add changes to overall structure

--- js/main.js
+++ js/main.js
@@ -2,7 +2,25 @@ import rallax from '../../rallax.js/dist/rallax.js'
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  rallax('.test1', './test-image.jpg', {
-    speed: 0.3,
+  rallax('.test1', './test-image3.jpg')
+
+  rallax('.test3', './test-image3.jpg', {
+    adjust: {
+      scale: 0.1,
+      offset: {
+        top: 0,
+        right: 0
+      }
+    }
+  })
+
+  rallax('.test4', './test-image3.jpg', {
+    adjust: {
+      scale: 0.1,
+      offset: {
+        top: 0,
+        left: 0
+      }
+    }
   })
 })
