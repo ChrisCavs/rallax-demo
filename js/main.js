ee2ecfed94e2c9bde5ee1dbc1b27073fbacdfa8a
Chris <chris211321@gmail.com>
2018-11-10 15:42:32 -0500

    top demo done, need test

--- js/main.js
+++ js/main.js
@@ -1,26 +1,10 @@
 import rallax from 'rallax.js'
 
 document.addEventListener('DOMContentLoaded', () => {
+  const dead = rallax('.dead')
+  const simple = rallax('.simple', {speed: 0.6})
+  const parallax = rallax('.parallax', {speed: 0.4})
+  const scrolling = rallax('.scrolling', {speed: 0.7})
 
-  rallax('.test1', './test-image3.jpg')
-
-  rallax('.test3', './test-image3.jpg', {
-    adjust: {
-      scale: 0.1,
-      offset: {
-        top: 0,
-        right: 0
-      }
-    }
-  })
-
-  rallax('.test4', './test-image3.jpg', {
-    adjust: {
-      scale: 0.1,
-      offset: {
-        top: 0,
-        left: 0
-      }
-    }
-  })
+  
 })
