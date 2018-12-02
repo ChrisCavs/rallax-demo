2ad9c1d9d1a0fae1d0a0e6ed265c7ccf2b3aaaf5
Christopher Cavalea <chris211321@gmail.com>
2018-12-02 16:48:14 -0500

    cleanup

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
-import rallax from 'rallax.js'
+import rallax from '/Users/Chris/Documents/libraries/rallax.js/dist/rallax.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const dead = rallax('.dead')
-  const simple = rallax('.simple', {speed: 0.6})
-  const parallax = rallax('.parallax', {speed: 0.4})
-  const scrolling = rallax('.scrolling', {speed: 0.7})
-
+  const secondBlock = document.querySelectorAll('.description')[1]
   
+  const stopTop = () => {
+    return secondBlock.getBoundingClientRect().top < 200
+  }
+
+  const startTop = () => {
+    return secondBlock.getBoundingClientRect().top >= 200
+  }
+
+  const dead = rallax('.dead', {disable: stopTop, enable: startTop})
+  const simple = rallax('.simple', { speed: 0.6, disable: stopTop, enable: startTop})
+  const parallax = rallax('.parallax', { speed: 0.4, disable: stopTop, enable: startTop})
+  const scrolling = rallax('.scrolling', { speed: 0.5, disable: stopTop, enable: startTop})
+
+
 })
