0b7f06786755810dbea7faa7a7f74f260087aa0e
Christopher Cavalea <chris211321@gmail.com>
2018-12-07 14:58:28 -0500

    update to refresh v2

--- js/main.js
+++ js/main.js
@@ -1,20 +1,77 @@
-import rallax from '/Users/Chris/Documents/libraries/rallax.js/dist/rallax.js'
+import rallax from 'rallax.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const secondBlock = document.querySelectorAll('.description')[1]
+  window.onbeforeunload = () => {
+    window.scrollTo(0, 0)
+  }
+
+  const dead = rallax('.dead')
+  const simple = rallax('.simple')
+  const parallax = rallax('.parallax')
+  const scrolling = rallax('.scrolling')
+
+  const objects = [dead, simple, parallax, scrolling]
+
+  dead.when(
+    () => dead.scrollY() > 320,
+    () => dead.changeSpeed(1.1)
+  ).when(
+    () => dead.scrollY() <= 320,
+    () => dead.changeSpeed(0.5)
+  )
+
+  simple.when(
+    () => simple.scrollY() > 320,
+    () => simple.changeSpeed(0.95)
+  ).when(
+    () => simple.scrollY() <= 320,
+    () => simple.changeSpeed(0.6)
+  )
+
+  parallax.when(
+    () => parallax.scrollY() > 320,
+    () => parallax.changeSpeed(0.8)
+  ).when(
+    () => parallax.scrollY() <= 320,
+    () => parallax.changeSpeed(0.7)
+  )
+
+  scrolling.when(
+    () => scrolling.scrollY() > 320,
+    () => scrolling.changeSpeed(0.64)
+  ).when(
+    () => scrolling.scrollY() <= 320,
+    () => scrolling.changeSpeed(0.8)
+  )
+
+  objects.forEach(obj => {
+      obj.when(
+        () => obj.scrollY() > 750,
+        () => obj.stop()
+      )
+      obj.when(
+        () => obj.scrollY() <= 750,
+        () => obj.start()
+      )
+    })
   
-  const stopTop = () => {
-    return secondBlock.getBoundingClientRect().top < 200
-  }
+  const image = document.querySelector('.parallax-image')
+  const imageContainer = document.querySelector('.container')
+  const imageContainerRect = imageContainer.getBoundingClientRect()
+  const winHeight = window.innerHeight
 
-  const startTop = () => {
-    return secondBlock.getBoundingClientRect().top >= 200
-  }
+  const imageTop = -(window.innerHeight * 0.3) * 2
+  const imageHeight = imageContainerRect.height + (winHeight * 0.3)
+  const imageWidth = imageContainerRect.width + 200
+  const imageLeft = -100
 
-  const dead = rallax('.dead', {disable: stopTop, enable: startTop})
-  const simple = rallax('.simple', { speed: 0.6, disable: stopTop, enable: startTop})
-  const parallax = rallax('.parallax', { speed: 0.4, disable: stopTop, enable: startTop})
-  const scrolling = rallax('.scrolling', { speed: 0.5, disable: stopTop, enable: startTop})
+  Object.assign(image.style, {
+    position: 'relative',
+    top: `${imageTop}px`,
+    left: `${imageLeft}px`,
+    height: `${imageHeight}px`,
+    width: `${imageWidth}px`,
+  })
 
-
+  const imageParallax = rallax(image)
 })
